ad6210dd8cf81f46a049fc56b8e48a0d4cbdbf47
Lucas Hemmingsen <99349701+LucasHemm@users.noreply.github.com>
2024-11-14 09:36:40 +0100

    Update UnitTest1.cs

--- UserService/UserServiceTest/UnitTest1.cs
+++ UserService/UserServiceTest/UnitTest1.cs
@@ -5,11 +5,11 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        int one = 1;
+        int one = 2;
         int two = 2;
 
         int three = one + two;
         
-        Assert.Equal(3,three);
+        Assert.Equal(4,three);
     }
 }
