2581ca35a50fe74b26e7c4de81d99379aae4dcb1
Matias Rottbøll <mrottboell@gmail.com>
2024-11-21 10:51:32 +0100

    cors

--- UserService/UserService/Program.cs
+++ UserService/UserService/Program.cs
@@ -22,6 +22,17 @@ public class Program
 // Enable Swagger/OpenAPI
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", policy =>
+            {
+                policy.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
+
 
         var app = builder.Build();
 
@@ -40,6 +51,7 @@ public class Program
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1"));
         }
 
+        app.UseCors("AllowAll");
         app.UseHttpsRedirection();
         app.UseAuthorization();
         app.MapControllers();
