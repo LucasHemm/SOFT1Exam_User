155a5ecebd7287792d09a4eaf5e07d2bafe38225
Lucas Hemmingsen <99349701+LucasHemm@users.noreply.github.com>
2024-11-21 13:31:24 +0100

    setup of docker and prometheus and grafana and dockerifle

--- UserService/UserService/Program.cs
+++ UserService/UserService/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Prometheus;
 using UserService.Facades;
 
 namespace UserService;
@@ -30,7 +31,15 @@ public class Program
             var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
             // Apply any pending migrations and create the database if it doesn't exist
-            dbContext.Database.Migrate();
+            try
+            {
+                dbContext.Database.Migrate();
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         // Configure the HTTP request pipeline.
@@ -39,8 +48,12 @@ public class Program
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1"));
         }
+        
+        app.UseRouting();
+        app.UseMetricServer(); // Default /metrics endpoint
+        app.UseHttpMetrics(); // Enable HttpMetrics
 
-        app.UseHttpsRedirection();
+        
         app.UseAuthorization();
         app.MapControllers();
 
